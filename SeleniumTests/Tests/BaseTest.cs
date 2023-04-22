46911bcc6d913e57a81618f5578a6e63cb6bb5f7
SantanaSJ <63861416+SantanaSJ@users.noreply.github.com>
2023-04-22 20:42:38 +0300

    Add files via upload

---
+++ SeleniumTests/Tests/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests.Tests
+{
+    public class BaseTest
+    {
+        protected WebDriver driver;
+        private ChromeOptions options;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.options = new ChromeOptions();
+            this.options.AddArgument("--headless");
+            this.driver = new ChromeDriver(this.options);
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Quit();
+        }
+    }
+}
